38d9bee9a346d65d338a874b42a1f7581dec6709
Antonios Pavlakis <antonis@pavlakis.info>
2017-04-26 23:44:26 +0100

    Add CspMiddleware

---
+++ .php_cs
@@ -0,0 +1,13 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+;
+
+return PhpCsFixer\Config::create()
+    ->setRules(array(
+        '@PSR2' => true,
+        'single_import_per_statement' => false
+    ))
+    ->setFinder($finder)
+;
---
+++ src/CspMiddleware.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Content Security Policy Middleware
+ *
+ * Add Content-Security-Policy headers for PSR-7 requests.
+ * Uses the csp-builder library paragonie/csp-builder.
+ *
+ * @link        https://github.com/pavlakis/csp-middleware
+ * @copyright   Copyright © 2017 Antonios Pavlakis
+ * @author      Antonios Pavlakis
+ * @license     https://github.com/pavlakis/csp-middleware/blob/master/LICENSE (MIT)
+ */
+namespace Pavlakis\Middleware\Csp;
+
+use ParagonIE\CSPBuilder\CSPBuilder;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class CspMiddleware
+{
+    const CSP_HEADER_ENABLE = 'Content-Security-Policy';
+    const CSP_HEADER_REPORT_ONLY = 'Content-Security-Policy';
+
+    /**
+     * @var CSPBuilder
+     */
+    private $cspBuilder;
+
+    /**
+     * @var bool
+     */
+    private $reportOnly;
+
+    /**
+     * CspMiddleware constructor.
+     * @param CSPBuilder $cspBuilder
+     * @param bool $reportOnly
+     */
+    public function __construct(CSPBuilder $cspBuilder, $reportOnly = true)
+    {
+        $this->cspBuilder = $cspBuilder;
+        $this->reportOnly = $reportOnly;
+    }
+
+    /**
+     * Invoke middleware
+     *
+     * @param  ServerRequestInterface   $request  PSR7 request object
+     * @param  ResponseInterface        $response PSR7 response object
+     * @param  callable                 $next     Next middleware callable
+     *
+     * @return ResponseInterface PSR7 response object
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        $header = static::CSP_HEADER_ENABLE;
+        if ($this->reportOnly) {
+            $header = static::CSP_HEADER_REPORT_ONLY;
+        }
+
+        $policies = $this->cspBuilder->compile();
+
+        $cspResponse = $response->withAddedHeader($header, $policies);
+
+        return $next($request, $cspResponse);
+    }
+}
