5ff9703fe4734eae180cf9c5cf446b3637b235f4
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2022-11-22 23:21:43 +0000

    Update tests compatibility with PHPUnit 9

--- tests/phpunit/CspMiddlewareTest.php
+++ tests/phpunit/CspMiddlewareTest.php
@@ -31,19 +31,20 @@ class CspMiddlewareTest extends TestCase
     protected function setUp(): void
     {
         $this->request = RequestFactory::createRequest();
-        $this->mockResponse = $this->getMockBuilder(ResponseInterface::class)->setMethods(['withAddedHeader']);
+        $this->mockResponse = $this->getMockForAbstractClass(ResponseInterface::class);
     }
 
-    public function test_report_only_header_with_csp_policies()
+    public function test_report_only_header_with_csp_policies(): void
     {
         $cspBuilder = CSPBuilder::fromFile(__DIR__.'/fixtures/csp.json');
         $cspMiddleware = new CspMiddleware($cspBuilder, true);
 
         $request = $this->request;
-        $response = $this->mockResponse->getMockForAbstractClass();
+        $response = $this->mockResponse;
         $response->expects(static::once())
             ->method('withAddedHeader')
-            ->with(static::equalTo('Content-Security-Policy-Report-Only'), static::equalTo("base-uri 'none'; default-src 'none'; child-src; connect-src 'none'; font-src 'self'; form-action 'self'; frame-ancestors 'none'; img-src 'self'; media-src 'none'; object-src 'none'; script-src 'self' https://www.google-analytics.com; style-src 'self'; report-uri /csp/enforce; upgrade-insecure-requests"));
+            ->with(static::equalTo('Content-Security-Policy-Report-Only'), static::equalTo("base-uri 'none'; default-src 'none'; child-src; connect-src 'none'; font-src 'self'; form-action 'self'; frame-ancestors 'none'; img-src 'self'; media-src 'none'; object-src 'none'; script-src 'self' https://www.google-analytics.com; style-src 'self'; report-uri /csp/enforce; upgrade-insecure-requests"))
+            ->willReturn($response);
 
         $next = function ($request, $response) {
             return $response;
@@ -52,16 +53,17 @@ class CspMiddlewareTest extends TestCase
         $cspMiddleware($request, $response, $next);
     }
 
-    public function test_report_header()
+    public function test_report_header(): void
     {
         $cspBuilder = CSPBuilder::fromFile(__DIR__.'/fixtures/csp.json');
         $cspMiddleware = new CspMiddleware($cspBuilder, false);
 
         $request = $this->request;
-        $response = $this->mockResponse->getMockForAbstractClass();
+        $response = $this->mockResponse;
         $response->expects(static::once())
             ->method('withAddedHeader')
-            ->with('Content-Security-Policy');
+            ->with('Content-Security-Policy')
+            ->willReturn($response);
 
         $next = function ($request, $response) {
             return $response;
